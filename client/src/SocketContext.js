aed45738cf7945d95de06a14963acb3451eab664
Kostiantyn Oliinyk <94046694+Kot1982@users.noreply.github.com>
2022-11-22 21:48:27 +0200

    change reference

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -1,57 +1,57 @@
-import React, { createContext, useState, useRef, useEffect } from 'react';
-import { io } from 'socket.io-client';
-import Peer from 'simple-peer';
+import React, { createContext, useState, useRef, useEffect } from 'react'
+import { io } from 'socket.io-client'
+import Peer from 'simple-peer'
 
-const SocketContext = createContext();
+const SocketContext = createContext()
 
-const socket = io('https://my-web-rtc-chat-app.herokuapp.com/');
+const socket = io('https://my-web-rtc-chat-app.onrender.com/')
 
 const ContextProvider = ({ children }) => {
-  const [stream, setStream] = useState(null);
-  const [me, setMe] = useState('');
-  const [call, setCall] = useState({});
-  const [callAccepted, setCallAccepted] = useState(false);
-  const [callEnded, setCallEnded] = useState(false);
-  const [name, setName] = useState('');
+  const [stream, setStream] = useState(null)
+  const [me, setMe] = useState('')
+  const [call, setCall] = useState({})
+  const [callAccepted, setCallAccepted] = useState(false)
+  const [callEnded, setCallEnded] = useState(false)
+  const [name, setName] = useState('')
 
-  const myVideo = useRef();
-  const userVideo = useRef();
-  const connectionRef = useRef();
+  const myVideo = useRef()
+  const userVideo = useRef()
+  const connectionRef = useRef()
 
   useEffect(() => {
     navigator.mediaDevices
       .getUserMedia({ video: true, audio: true })
       .then((currentStream) => {
-        setStream(currentStream);
-        myVideo.current.srcObject = currentStream;
-      });
+        setStream(currentStream)
+        myVideo.current.srcObject = currentStream
+      })
 
-    socket.on('me', (id) => setMe(id));
+    socket.on('me', (id) => setMe(id))
 
     socket.on('calluser', ({ from, name: callerName, signal }) => {
-      setCall({ isReceivedCall: true, from, name: callerName, signal });
-    });
-  }, []);
+      setCall({ isReceivedCall: true, from, name: callerName, signal })
+    })
+  }, [])
 
   const answerCall = () => {
-    setCallAccepted(true);
+    setCallAccepted(true)
 
-    const peer = new Peer({ initiator: false, trickle: false, stream });
+    const peer = new Peer({ initiator: false, trickle: false, stream })
 
     peer.on('signal', (data) => {
-      socket.emit('answercall', { signal: data, to: call.from });
-    });
+      socket.emit('answercall', { signal: data, to: call.from })
+    })
 
     peer.on('stream', (currentStream) => {
-      userVideo.current.srcObject = currentStream;
-    });
+      userVideo.current.srcObject = currentStream
+    })
 
-    peer.signal(call.signal);
+    peer.signal(call.signal)
 
-    connectionRef.current = peer;
-  };
+    connectionRef.current = peer
+  }
   const callUser = (id) => {
-    const peer = new Peer({ initiator: true, trickle: false, stream });
+    const peer = new Peer({ initiator: true, trickle: false, stream })
 
     peer.on('signal', (data) => {
       socket.emit('calluser', {
@@ -59,28 +59,28 @@ const ContextProvider = ({ children }) => {
         signalData: data,
         from: me,
         name,
-      });
-    });
+      })
+    })
 
     peer.on('stream', (currentStream) => {
-      userVideo.current.srcObject = currentStream;
-    });
+      userVideo.current.srcObject = currentStream
+    })
 
     socket.on('callaccepted', (signal) => {
-      setCallAccepted(true);
+      setCallAccepted(true)
 
-      peer.signal(signal);
-    });
+      peer.signal(signal)
+    })
 
-    connectionRef.current = peer;
-  };
+    connectionRef.current = peer
+  }
   const leaveCall = () => {
-    setCallEnded(true);
+    setCallEnded(true)
 
-    connectionRef.current.destroy();
+    connectionRef.current.destroy()
 
-    window.location.reload();
-  };
+    window.location.reload()
+  }
 
   return (
     <SocketContext.Provider
@@ -101,7 +101,7 @@ const ContextProvider = ({ children }) => {
     >
       {children}
     </SocketContext.Provider>
-  );
-};
+  )
+}
 
-export { ContextProvider, SocketContext };
+export { ContextProvider, SocketContext }
